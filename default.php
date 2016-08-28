542a670ec19daf51909cba42531697175ca2cb2e
Rei Mark <Rei Mark>
2016-08-28 14:15:42 +0800

    first commit

---
+++ default.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML>
+<!--
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Daily Digital by Rei</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body id="top">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="images/avatar-1.jpg" alt="" /></a>
+					<h1><strong>I am Rei</strong>, a Web Systems Developer<br />
+					from Cebu, Philippines.</h1>
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- One -->
+					<section id="one">
+						<header class="major">
+							<h2>Expert Website Development.</h2>
+						</header>
+						<p>I help people turn their ideas into outstanding websites:
+						<ul>
+							<li>business owners who need brand-new websites, or major improvements to existing ones</li>
+							<li>entrepreneurs who want profitable online stores with secure e-commerce</li>
+							<li>nonprofit organizations that need websites for raising money and building online communities</li>
+							<li>web design agencies and freelancers seeking website programming expertise for collaborative projects</li>
+						</ul>
+						</p>
+					</section>
+
+				<!-- Two -->
+					<section id="two">
+						<h2>Recent Work</h2>
+						<div class="row">
+							<article class="6u 12u$(xsmall) work-item">
+								<h3><a href="https://hgs-booking.com/gs2015stage1/login.html">Good Sports</a></h3>
+								<p>A management system for an outdoor sports company.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<h3><a href="http://lamlamguam.com">LamLam Bus</a></h3>
+								<p>A bus ticketing system for Lamlam Tours.</p>
+							</article>
+							<article class="6u 12u$(xsmall) work-item">
+								<h3><a href="http://hireapp.com">HireApp</a></h3>
+								<p>A service booking system (ex: GrabTaxi) for Lamlam Tours.</p>
+							</article>
+							<article class="6u 12u$(xsmall) work-item">
+								<h3><a href="http://www.dranix.com.ph">Dranix Distributors Inc.</a></h3>
+								<p>An official website of Dranix company.</p>
+							</article>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three">
+						<h2>Get In Touch</h2>
+						<p></p>
+						<div class="row">
+							<div class="8u 12u$(small)">
+								<form method="post" action="#">
+									<div class="row uniform 50%">
+										<div class="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
+										<div class="6u$ 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+										<div class="12u$"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+									</div>
+								</form>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" /></li>
+								</ul>
+							</div>
+							<div class="4u$ 12u$(small)">
+								<ul class="labeled-icons">
+									<li>
+										<h3 class="icon fa-home"><span class="label">Address</span></h3>
+										Tungha-an Minglanilla<br />
+										Cebu, 6000<br />
+										Philippines.
+									</li>
+									<li>
+										<h3 class="icon fa-mobile"><span class="label">Phone</span></h3>
+										(+63)032-263-7668
+									</li>
+									<li>
+										<h3 class="icon fa-envelope-o"><span class="label">Email</span></h3>
+										<a href="#">rmmedillo@protonmail.com</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="https://twitter.com/rmmedillo" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://github.com/reymedillo" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="mailto:rmmedillo@protonmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; rmmedillo</li><li>Design: <a href="http://html5up.net">Rei</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
